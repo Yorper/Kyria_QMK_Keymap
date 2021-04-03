128451f485f3d40006ddbddf4d3113d1b97344d7
Yorper <david@majinbuu.com>
2021-04-03 21:06:07 +1100

    initial commit

---
+++ rules.mk
@@ -0,0 +1,7 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+CONSOLE_ENABLE = no
+
+WPM_ENABLE = yes
