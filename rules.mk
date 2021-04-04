60a7d6f0ddeb6d547566e63dc34cc55b098389d8
Yorper <david@majinbuu.com>
2021-04-04 17:53:35 +1000

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -2,5 +2,5 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 
-CONSOLE_ENABLE = no        # Disables console/debug to free memory
 WPM_ENABLE = yes           # Enables WPM counter
+CONSOLE_ENABLE = no        # Disables console/debug to free memory
